a0607ccd8db4930531fc2ed631c7480ac9a29345
Peter <Peter.Geisler1959@gmail.com>
2026-08-16 08:32:54 +0200

    Add GPS data to BikeHello

---
+++ ios/App/App/GPSPlugin.swift
@@ -0,0 +1,82 @@
+﻿import Foundation
+import Capacitor
+import CoreLocation
+
+@objc(GPSPlugin)
+public class GPSPlugin: CAPPlugin, CAPBridgedPlugin, CLLocationManagerDelegate {
+
+    public let identifier = "GPSPlugin"
+    public let jsName = "GPS"
+
+    public let pluginMethods: [CAPPluginMethod] = [
+        CAPPluginMethod(name: "start", returnType: CAPPluginReturnNone),
+        CAPPluginMethod(name: "stop", returnType: CAPPluginReturnNone)
+    ]
+
+    private let locationManager = CLLocationManager()
+
+    override public func load() {
+        super.load()
+
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+    }
+
+    @objc func start(_ call: CAPPluginCall) {
+
+        notifyListeners("gpsDebug", data: [
+            "message": "GPS: start() erreicht"
+        ])
+
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+
+        call.resolve()
+    }
+
+    @objc func stop(_ call: CAPPluginCall) {
+
+        locationManager.stopUpdatingLocation()
+
+        notifyListeners("gpsDebug", data: [
+            "message": "GPS: gestoppt"
+        ])
+
+        call.resolve()
+    }
+
+    public func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+
+        guard let location = locations.last else {
+            return
+        }
+
+        let speed = location.speed >= 0 ? location.speed : 0
+        let course = location.course >= 0 ? location.course : 0
+
+        notifyListeners("gpsData", data: [
+            "lat": location.coordinate.latitude,
+            "lon": location.coordinate.longitude,
+            "speed": speed,
+            "course": course,
+            "horizontalAccuracy": location.horizontalAccuracy,
+            "altitude": location.altitude,
+            "verticalAccuracy": location.verticalAccuracy,
+            "timestamp": location.timestamp.timeIntervalSince1970
+        ])
+    }
+
+    public func locationManager(
+        _ manager: CLLocationManager,
+        didFailWithError error: Error
+    ) {
+
+        notifyListeners("gpsDebug", data: [
+            "message": "GPS Fehler: \(error.localizedDescription)"
+        ])
+    }
+}
--- ios/App/App/MainViewController.swift
+++ ios/App/App/MainViewController.swift
@@ -1,8 +1,9 @@
-﻿import Capacitor
+import Capacitor
 
 class MainViewController: CAPBridgeViewController {
 
     override func capacitorDidLoad() {
         bridge?.registerPluginInstance(MagnetometerPlugin())
+        bridge?.registerPluginInstance(GPSPlugin())
     }
 }
