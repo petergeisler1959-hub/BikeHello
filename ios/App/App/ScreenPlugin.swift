cafb8c7c16a70793490a5cdaa2e73cc5d0ffb487
Peter <Peter.Geisler1959@gmail.com>
2026-09-03 12:49:09 +0200

    Add screen keep-awake logic

---
+++ ios/App/App/ScreenPlugin.swift
@@ -0,0 +1,49 @@
+﻿import UIKit
+import Capacitor
+
+@objc(ScreenPlugin)
+public class ScreenPlugin: CAPPlugin, CAPBridgedPlugin {
+
+    public let identifier = "ScreenPlugin"
+    public let jsName = "Screen"
+
+    public let pluginMethods: [CAPPluginMethod] = [
+        CAPPluginMethod(
+            name: "setKeepScreenOn",
+            returnType: CAPPluginReturnNone
+        )
+    ]
+
+    override public func load() {
+        super.load()
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(appDidEnterBackground),
+            name: UIApplication.didEnterBackgroundNotification,
+            object: nil
+        )
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    @objc func setKeepScreenOn(_ call: CAPPluginCall) {
+
+        let enabled =
+            call.getBool("enabled") ?? false
+
+        DispatchQueue.main.async {
+            UIApplication.shared.isIdleTimerDisabled = enabled
+            call.resolve()
+        }
+    }
+
+    @objc private func appDidEnterBackground() {
+
+        DispatchQueue.main.async {
+            UIApplication.shared.isIdleTimerDisabled = false
+        }
+    }
+}
